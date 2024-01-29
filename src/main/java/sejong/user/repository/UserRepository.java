158943fb00b509b0093dc67141b0193df7610991
jaepyo99 <jaepyo99@naver.com>
2024-01-30 02:58:15 +0900

    feat: 팀에 속한 사용자 목록 조회
    득점왕과 같은 칭호 추가안함. erd 관련 회의 진행 필요

--- src/main/java/sejong/user/repository/UserRepository.java
+++ src/main/java/sejong/user/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package sejong.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sejong.user.entity.User;
 
-public interface UserRepository extends JpaRepository<User,String> {
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User,Long> {
     User save(User entity);
 
     @Override
     void deleteAll();
+
+    @Query("select u from user_tb as u where u.id in :usersId")
+    List<User> findUsersIn(@Param("usersId") List<Long>usersId);
 }
--- src/main/java/sejong/user/service/UserTeamService.java
+++ src/main/java/sejong/user/service/UserTeamService.java
@@ -2,9 +2,12 @@ package sejong.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sejong.user.entity.User;
 import sejong.user.entity.UserTeam;
+import sejong.user.repository.UserRepository;
 import sejong.user.repository.UserTeamRepository;
 import sejong.user.service.dto.SizeUserTeamDto;
+import sejong.user.service.res.UsersInfoInTeamResponseDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,9 +16,11 @@ import java.util.stream.Collectors;
 @Service
 public class UserTeamService {
     private final UserTeamRepository userTeamRepository;
+    private final UserRepository userRepository;
 
     /**
      * 특정 팀에 소속된 사람의 수를 구해주는 메서드
+     *
      * @param teamId
      * @return SizeUserTeamDto
      */
@@ -24,5 +29,20 @@ public class UserTeamService {
                 .filter(userTeam -> userTeam.getAccept().booleanValue())
                 .collect(Collectors.toList());
         return SizeUserTeamDto.of(allByTeamId.size());
+    }
+
+    public List<UsersInfoInTeamResponseDto> findMembersInfoInTeam(Long teamId) {
+        List<UserTeam> membersInTeam = userTeamRepository.findMembersInTeamByTeamId(teamId);
+
+        List<Long> membersId = membersInTeam.stream().map(UserTeam::getUserId).collect(Collectors.toList());
+        List<User> usersIn = userRepository.findUsersIn(membersId);
+        return usersIn.stream().map(user ->
+                UsersInfoInTeamResponseDto.builder()
+                        .teamCnt(userTeamRepository.findTeamsInUserByUserId(user.getId()).size())
+                        .userName(user.getName())
+                        .userId(user.getId())
+                        .position(user.getPosition())
+                        .age(user.getAge())
+                        .build()).collect(Collectors.toList());
     }
 }
