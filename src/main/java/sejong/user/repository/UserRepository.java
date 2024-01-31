0226b05a155e4932ed8e93cfe4b0dd0182ff141b
euics <pjh575157@gmail.com>
2024-01-31 17:06:16 +0900

    feat: Add Method #15
    findByStudentId Method
    - To Find User by StudentId
    - Factor: studentId, Return: Optional<User>

--- src/main/java/sejong/user/repository/UserRepository.java
+++ src/main/java/sejong/user/repository/UserRepository.java
@@ -3,9 +3,13 @@ package sejong.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sejong.user.entity.User;
 
-public interface UserRepository extends JpaRepository<User,String> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
     User save(User entity);
 
     @Override
     void deleteAll();
+
+    Optional<User> findByStudentId(String studentId);
 }
