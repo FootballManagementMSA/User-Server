74f89c9520205e1de33d72c19f6fcb1c527f7e42
euics <pjh575157@gmail.com>
2024-01-31 19:01:29 +0900

    feat: Add Constants #17

--- src/main/java/sejong/user/global/exception/constant/StatusCodeConstant.java
+++
@@ -1,5 +0,0 @@
-package sejong.user.global.exception.constant;
-
-public class StatusCodeConstant {
-    public static final Integer NOT_FOUND_STATUS_CODE = 404;
-}
--- src/main/java/sejong/user/service/constant/UserAuthServiceConstant.java
+++ src/main/java/sejong/user/service/constant/UserAuthServiceConstant.java
@@ -8,4 +8,9 @@ public class UserAuthServiceConstant {
     public static final String PASSWORD = "pw";
     public static final String AUTH_FAIL = "auth_failed";
     public static final String SUCCESS = "success";
+    public static final String BODY = "body";
+    public static final String GRADE = "grade";
+    public static final String MAJOR = "major";
+    public static final String NAME = "name";
+    public static final String STATUS = "status";
 }
