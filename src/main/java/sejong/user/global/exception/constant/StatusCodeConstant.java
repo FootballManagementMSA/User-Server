90ccc177a4ad42c347c6038326b9f3c65992ecf9
euics <pjh575157@gmail.com>
2024-01-31 17:05:05 +0900

    feat: Add Constant Class #15
    ExceptionMessageConstant
    - Manage ExceptionMessage
    StatusCodeConstant
    - Manage StatusCode
    UserAuthServiceConstant
    - Manage AuthService Constant

---
+++ src/main/java/sejong/user/global/exception/constant/StatusCodeConstant.java
@@ -0,0 +1,5 @@
+package sejong.user.global.exception.constant;
+
+public class StatusCodeConstant {
+    public static final Integer NOT_FOUND_STATUS_CODE = 404;
+}
---
+++ src/main/java/sejong/user/service/constant/UserAuthServiceConstant.java
@@ -0,0 +1,11 @@
+package sejong.user.service.constant;
+
+public class UserAuthServiceConstant {
+    public static final String RESULT = "RESULT";
+    public static final String CODE = "CODE";
+    public static final String SEJONG_AUTH_URL = "https://auth.imsejong.com/auth?method=ClassicSession";
+    public static final String ID = "id";
+    public static final String PASSWORD = "pw";
+    public static final String AUTH_FAIL = "auth_failed";
+    public static final String SUCCESS = "success";
+}
