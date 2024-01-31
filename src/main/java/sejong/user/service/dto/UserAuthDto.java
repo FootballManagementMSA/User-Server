be2a7fea6069ea15e74bf56a84d2c010017ee81f
euics <pjh575157@gmail.com>
2024-01-31 19:05:50 +0900

    feat: Add Dto Inner Class #17
    UserRegisterRequest
    - Register Request Class
    UserDto
    - Dto class of User

--- src/main/java/sejong/user/service/dto/UserAuthDto.java
+++ src/main/java/sejong/user/service/dto/UserAuthDto.java
@@ -1,5 +1,6 @@
 package sejong.user.service.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,39 @@ public class UserAuthDto {
 
     @Getter
     @Setter
+    @Builder
     public static class UserAuthRequest {
         private String id;
         private String pw;
+    }
+
+    @Getter
+    @Setter
+    public static class UserRegisterRequest{
+        private String studentId;
+        private String password;
+        private String position;
+        private String foot;
+        private String sex;
+        private Integer age;
+        private Integer height;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class UserDto{
+        private String studentId;
+        private String password;
+        private String grade;
+        private String status;
+        private String major;
+        private String name;
+
+        private String position;
+        private String foot;
+        private String sex;
+        private Integer age;
+        private Integer height;
     }
 }
