b1f4f9ae5c8f2c90c1a9dcb163b37aee7e369d6b
euics <pjh575157@gmail.com>
2024-02-07 15:20:59 +0900

    feat: Add Exception Message #13

--- src/main/java/sejong/user/global/exception/constant/ExceptionMessageConstant.java
+++ src/main/java/sejong/user/global/exception/constant/ExceptionMessageConstant.java
@@ -4,4 +4,5 @@ public class ExceptionMessageConstant {
     public static final String SEJONG_AUTH_FAIL_EXCEPTION_MESSAGE = "세종대학교 재학생이 아닙니다.";
     public static final String NOT_REGISTER_USER_EXCEPTION_MESSAGE = "회원가입 되지 않은 학생입니다.";
     public static final String ALREADY_REGISTERED_USER = "이미 가입된 회원입니다.";
+    public static final String ID_PASSWORD_MISMATCH_EXCEPTION_MESSAGE = "아이디 비밀번호가 일치하지 않습니다.";
 }
