4b0bd231f94e1c190b94f4121fb0ce35916b7aa6
jaepyo99 <jaepyo99@naver.com>
2024-02-13 20:37:28 +0900

    refactory: 특정팀에 속한 사용자 에러
    - 팀에 속한 사용자 조회시 사용자가 속해있는 팀의 수가 맞지않는 에러 수정

--- src/main/java/sejong/user/service/UserTeamService.java
+++ src/main/java/sejong/user/service/UserTeamService.java
@@ -44,11 +44,17 @@ public class UserTeamService {
     }
 
     public List<UsersInfoInTeamResponseDto> findMembersInfoInTeam(Long teamId) {
-        List<UserTeam> membersInTeam = userTeamRepository.findMembersInTeamByTeamId(teamId);
-        List<User> users = membersInTeam.stream().map(UserTeam::getUser).collect(Collectors.toList());
+        List<UserTeam> membersInTeam = userTeamRepository.findMembersInTeamByTeamId(teamId); // 특정팀에 가입대기열
+
+
+        List<User> users = membersInTeam.stream()
+                .filter(userTeam -> userTeam.getAccept().equals(true))
+                .map(UserTeam::getUser)
+                .collect(Collectors.toList()); // 특정팀에 속한 사용자
+
         return users.stream().map(user ->
                 UsersInfoInTeamResponseDto.builder()
-                        .teamCnt(users.size())
+                        .teamCnt(getTeamCnt(user))
                         .userName(user.getName())
                         .userId(user.getId())
                         .position(user.getPosition())
@@ -56,29 +62,37 @@ public class UserTeamService {
                         .build()).collect(Collectors.toList());
     }
 
-    public List<ApplyUsersInfoResponseDto> findApplyMember(Long teamId) {
+    private int getTeamCnt(User user) {
+        return userTeamRepository.findByUserId(user.getId())
+                .stream()
+                .filter(userTeam -> userTeam.getAccept().equals(true))
+                .collect(Collectors.toList()).size();
+    }
+
+    public List<ApplyUsersInfoResponseDto> findApplicantInTeam(Long teamId) {
         List<UserTeam> applyMembers = userTeamRepository.findApplyMembersInTeamByTeamId(teamId);
-        return applyMembers.stream().map(userTeam -> {
-            User user = userTeam.getUser();
-            return ApplyUsersInfoResponseDto.builder()
-                    .userId(user.getId())
-                    .age(user.getAge())
-                    .teamCnt(applyMembers.size())
-                    .introduce(userTeam.getIntroduce())
-                    .userName(user.getName())
-                    .position(user.getPosition())
-                    .build();
-        }).collect(Collectors.toList());
+        return applyMembers.stream().filter(userTeam -> userTeam.getAccept() == false)
+                .map(userTeam -> {
+                    User user = userTeam.getUser();
+                    return ApplyUsersInfoResponseDto.builder()
+                            .userId(user.getId())
+                            .age(user.getAge())
+                            .teamCnt(applyMembers.size())
+                            .introduce(userTeam.getIntroduce())
+                            .userName(user.getName())
+                            .position(user.getPosition())
+                            .build();
+                }).collect(Collectors.toList());
     }
 
 
-    @KafkaListener(topics = "team",groupId = "group_1")
+    @KafkaListener(topics = "team", groupId = "group_1")
     public void applyMember(ApplyUserTeamInfoRequestDto userInfoRequestDto) {
         // 특정 team에 userId가 속한지 확인
         Optional<UserTeam> userTeam =
                 userTeamRepository.findByUserIdAndTeamId(userInfoRequestDto.getUserId(), userInfoRequestDto.getTeamId());
         // 있으면 예외
-        if(userTeam.isPresent()){
+        if (userTeam.isPresent()) {
             throw new IllegalStateException("이미 가입신청중인데 뭘 또 신청해 에러에러");
         }
         User user = userRepository.findById(userInfoRequestDto.getUserId()).orElseThrow(NullPointerException::new);
@@ -95,15 +109,15 @@ public class UserTeamService {
     }
 
     @Transactional
-    @KafkaListener(topics = "team_confirm",groupId = "group_1")
+    @KafkaListener(topics = "team_confirm", groupId = "group_1")
     public void confirmApplicant(ConfirmApplicationRequestDto requestDto) {
         // 특정 team에 userId가 속한지 확인
         UserTeam userTeam =
                 userTeamRepository.findByUserIdAndTeamId(requestDto.getUserId(), requestDto.getTeamId()).orElseThrow(NullPointerException::new);
-        if(userTeam.getAccept()){
+        if (userTeam.getAccept()) {
             throw new IllegalStateException("이미 가입된사람을 가입취소요청이 왜와");
         }
-        if(!requestDto.isApprove()){
+        if (!requestDto.isApprove()) {
             userTeamRepository.delete(userTeam);
         }
         userTeam.approve();
