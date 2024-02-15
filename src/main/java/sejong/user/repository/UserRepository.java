f9d34537feaf04955bedfd21144c778d9bec1eba
jaepyo99 <jaepyo99@naver.com>
2024-02-16 01:35:34 +0900

    refactor: 불필요한 로직 및 클래스 삭제
    - UserRepository 불필요한 메서드 삭제
    - UserTeamKafkaConsumer 삭제
    - ModelMapperConfig 삭제

--- src/main/java/sejong/user/repository/UserRepository.java
+++ src/main/java/sejong/user/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package sejong.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import sejong.user.entity.User;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
@@ -13,7 +10,5 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     @Override
     void deleteAll();
-    @Query("select u from user_tb as u where u.id in :usersId")
-    List<User> findUsersIn(@Param("usersId") List<Long>usersId);
     Optional<User> findByStudentId(String studentId);
 }
