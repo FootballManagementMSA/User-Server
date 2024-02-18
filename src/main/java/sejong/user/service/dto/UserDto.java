77e2558bfbaa08574d9d911c6f33efec246f7cef
euics <pjh575157@gmail.com>
2024-02-18 21:23:15 +0900

    feat: Add ModifyUserRequest Class #24
    - 회원 정보 수정 Request

--- src/main/java/sejong/user/service/dto/UserDto.java
+++ src/main/java/sejong/user/service/dto/UserDto.java
@@ -3,6 +3,7 @@ package sejong.user.service.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 public class UserDto {
 
@@ -12,5 +13,17 @@ public class UserDto {
         public String studentId;
         public String name;
         public String image;
+    }
+
+    @Getter
+    @Builder
+    public static class ModifyUserRequest {
+        public String name;
+        public Integer age;
+        public Integer height;
+        public String sex;
+        public String position;
+        public String foot;
+        public MultipartFile image;
     }
 }
