cd51546ccb01d91f3306b56572aa9474cc94ffb2
euics <pjh575157@gmail.com>
2024-03-05 01:28:40 +0900

    feat: 회원 탈퇴 API Kafka 연동 #25

---
+++ src/main/java/sejong/user/global/kafka/CommonJsonSerializer.java
@@ -0,0 +1,19 @@
+package sejong.user.global.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CommonJsonSerializer {
+    public static Map<String,Object> getStringObjectMap(String bootstrapServer) {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return config;
+    }
+}
