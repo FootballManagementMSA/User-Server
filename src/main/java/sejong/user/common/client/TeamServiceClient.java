2a32a9f6ece11eeeff189fdd6df90baffb7a28ce
euics <pjh575157@gmail.com>
2024-03-29 14:25:36 +0900

    hotfix: Change Method Get to Post

--- src/main/java/sejong/user/common/client/TeamServiceClient.java
+++ src/main/java/sejong/user/common/client/TeamServiceClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import sejong.user.common.client.dto.ScheduleInfoDto;
 import sejong.user.service.dto.MainDto;
@@ -15,6 +16,6 @@ public interface TeamServiceClient {
     @GetMapping("/api/team-service/{userId}/schedule")
     ResponseEntity<List<ScheduleInfoDto>> getScheduleInfo(@PathVariable(value = "userId") Long userId);
 
-    @GetMapping("/api/team-service/team-info")
+    @PostMapping("/api/team-service/team-info")
     ResponseEntity<List<MainDto.RegisteredTeamInfoResponse>> getRegisteredTeamInfo(@RequestBody List<Long> teamIds);
 }
