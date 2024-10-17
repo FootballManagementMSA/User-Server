5b8eef3215846b09724b02a6aed1e50bd248ded0
euics <pjh575157@gmail.com>
2024-10-17 20:29:44 +0900

    feat: Add UserDto for user data transfer objects
    사용자 데이터 전송 객체를 위한 UserDto 클래스 추가.
    - `GetUserResponse` 클래스: 학생 ID와 이름을 포함하여 사용자 정보를 응답하는 DTO 생성.
    - `ModifyUserRequest` 클래스: 학생 ID, 이름, 나이, 키, 성별, 포지션, 발 정보를 포함하여 사용자 정보 수정 요청을 위한 DTO 생성.

--- src/main/java/sejong/user/user/dto/UserDto.java
+++ src/main/java/sejong/user/user/dto/UserDto.java
@@ -2,28 +2,25 @@ package sejong.user.user.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 public class UserDto {
 
     @Getter
     @Builder
-    public static class MyPageResponse {
+    public static class GetUserResponse {
         public String studentId;
         public String name;
-        public String image;
     }
 
     @Getter
     @Builder
     public static class ModifyUserRequest {
+        public String studentId;
         public String name;
         public Integer age;
         public Integer height;
         public String sex;
         public String position;
         public String foot;
-        public MultipartFile image;
     }
 }
