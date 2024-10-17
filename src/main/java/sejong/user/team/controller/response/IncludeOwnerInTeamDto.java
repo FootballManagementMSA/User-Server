7f0ffc8bcb448a81dad2e0e42b8337922c0bb3b2
euics <pjh575157@gmail.com>
2024-10-17 20:23:36 +0900

    delete: Delete Code

--- src/main/java/sejong/user/team/controller/response/IncludeOwnerInTeamDto.java
+++
@@ -1,9 +0,0 @@
-package sejong.user.team.controller.response;
-
-import lombok.Getter;
-
-@Getter
-public class IncludeOwnerInTeamDto {
-    private Long teamId;
-    private String token;
-}
--- src/main/java/sejong/user/user/entity/Role.java
+++
@@ -1,9 +0,0 @@
-package sejong.user.user.entity;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public enum Role {
-    LEADER("동아리장"),MEMBER("동아리원");
-    private String krState;
-}
