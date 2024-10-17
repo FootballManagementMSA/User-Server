dee3b874a37a817b514f298af9670bc6ce633274
euics <pjh575157@gmail.com>
2024-10-17 20:28:44 +0900

    feat: Add UserRepository interface for user data access
    사용자 데이터 접근을 위한 UserRepository 인터페이스 추가.
    - `save` 메서드를 통해 사용자 엔티티를 저장하는 기능 제공.
    - `deleteAll` 메서드를 재정의하여 모든 사용자 데이터 삭제 기능 제공.
    - `findByStudentId` 메서드를 통해 학생 ID로 사용자 정보를 조회하는 기능 추가.

--- src/main/java/sejong/user/user/repository/UserRepository.java
+++ src/main/java/sejong/user/user/repository/UserRepository.java
@@ -2,16 +2,16 @@ package sejong.user.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sejong.user.user.entity.User;
+import sejong.user.user.entity.UserEntity;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User save(User entity);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity save(UserEntity entity);
 
     @Override
     void deleteAll();
 
-    Optional<User> findByStudentId(String studentId);
+    Optional<UserEntity> findByStudentId(String studentId);
 }
