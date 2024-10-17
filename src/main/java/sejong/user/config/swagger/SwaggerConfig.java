8a1870f430281432f2fbec1f9cc1391f7c15c6ce
euics <pjh575157@gmail.com>
2024-10-17 20:30:25 +0900

    feat: Add SwaggerConfig for API documentation
    API 문서화를 위한 SwaggerConfig 클래스 추가.
    - `openAPI` 메서드를 통해 OpenAPI 객체를 생성하여 Swagger 설정 구성.
    - `apiInfo` 메서드를 통해 API 제목, 설명, 버전 정보를 설정하여 API 문서화 정보 제공.

---
+++ src/main/java/sejong/user/config/swagger/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package sejong.user.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Spring Boot REST API Specifications")
+            .description("Specification")
+            .version("1.0.0");
+    }
+}
